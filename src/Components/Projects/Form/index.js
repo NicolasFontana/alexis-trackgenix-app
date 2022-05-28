2dc4b11f8c882289a848ae2890c1f3b9ca89eb1b
iaracriscenti <iaracriscenti@gmail.com>
2022-05-28 13:57:53 -0300

    TG-24: Created form folder and added route to layout

---
+++ src/Components/Projects/Form/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './form.module.css';
+
+const ProjectForm = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <h2>Form </h2>
+      </div>
+    </>
+  );
+};
+export default ProjectForm;
