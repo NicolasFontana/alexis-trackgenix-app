781706e1c6dfd0960d75a709eabb8e2847a860fd
LuliBianchini <lulibianchini97@gmail.com>
2022-05-31 19:06:04 -0300

    TG-32: Created list, delete and modal for success

--- src/Components/Employees/index.js
+++ src/Components/Employees/index.js
@@ -1,27 +1,30 @@
 import { useEffect, useState } from 'react';
 import styles from './employees.module.css';
+import List from './List/List';
 
-function Employees() {
-  const [employees, saveEmployees] = useState([]);
+const Employees = () => {
+  const [list, setEmployees] = useState([]);
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users`)
+    fetch(`${process.env.REACT_APP_API_URL}api/employees`)
       .then((response) => response.json())
       .then((response) => {
-        saveEmployees(response);
+        setEmployees(response.data);
+        console.log(response);
       });
   }, []);
 
+  const deleteItem = async (_id) => {
+    setEmployees([...list.filter((listItem) => listItem._id !== _id)]);
+  };
+
   return (
     <section className={styles.container}>
-      <h2>Employees</h2>
-      <div>
-        {employees.map((employee) => {
-          return <div key={employee.id}>{employee.name}</div>;
-        })}
-      </div>
+      <h2 className={styles.employees}> Employees </h2>
+      <List list={list} setEmployees={setEmployees} deleteItem={deleteItem} />
+      <button className={styles.addbtn}>&#10010; Add New Employee</button>
     </section>
   );
-}
+};
 
 export default Employees;
