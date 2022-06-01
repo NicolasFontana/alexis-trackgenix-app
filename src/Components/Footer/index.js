8625e171b35f03ea42f62f5954a394b0cd00697e
LuliBianchini <lulibianchini97@gmail.com>
2022-05-31 23:31:31 -0300

    TG-39: Header and Footer style according to figma model

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -29,27 +29,7 @@ function Footer() {
         </ul>
       </div>
       <div className={styles.license}>
-        <div className={styles.copyright}>Copyright © 2021 Radium Rocket</div>
-        <div>
-          <a href={'https://www.facebook.com/radiumrocket'} target={'_blank'} rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/facebook.svg`}
-            />
-          </a>
-          <a href={'https://twitter.com/radiumrocket'} target={'_blank'} rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/twitter.svg`}
-            />
-          </a>
-          <a href={'https://www.instagram.com/radium.rocket/'} target={'_blank'} rel="noreferrer">
-            <img
-              className={styles.socialIcon}
-              src={`${process.env.PUBLIC_URL}/assets/images/instagram.svg`}
-            />
-          </a>
-        </div>
+        <div className={styles.copyright}>Copyright © 2022 Trackgenix</div>
       </div>
     </footer>
   );
