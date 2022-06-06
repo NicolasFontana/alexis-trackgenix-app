89148970530b8652d225100bd8ab3bcc07d61d27
NicolasFontana <nico.fontana12@gmail.com>
2022-06-06 10:54:19 -0300

    TG-48: fixed error

--- src/Components/TimeSheets/List/List.jsx
+++ src/Components/TimeSheets/List/List.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ListItem from '../ListItem/ListItem';
-import styles from './list.module.css';
+import styles from './List.module.css';
 
 const List = ({ timeSheets, deleteItem, setShowModal }) => {
   return (
