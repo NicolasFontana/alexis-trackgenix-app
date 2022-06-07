0c217babbbe047b9d1c95e91c4c4ed119470f11d
alejogf <agonzalezfittipaldi@gmail.com>
2022-06-07 15:00:14 -0300

    TG-44: base of the error and success modals

--- src/Components/Shared/SuccessModal/successmodal.jsx
+++ src/Components/Shared/SuccessModal/successmodal.jsx
@@ -1,6 +1,32 @@
-// import React from 'react';
-// import styles from './successmodal.module.css';
+import React from 'react';
+import styles from './successModal.module.css';
 
-// const successModal
+const successModal = ({ showModal, setShowModal }) => {
+  if (!showModal) {
+    return null;
+  }
+  return (
+    <div className={styles.modalBackground}>
+      <div className={styles.modalContainer}>
+        <div className={styles.modalCloseBtn}>
+          <button className={styles.btnClose} onClick={() => setShowModal(false)}>
+            X
+          </button>
+        </div>
+        <div className={styles.title}>
+          <h1>Great!</h1>
+        </div>
+        <div className={styles.description}>
+          <p>Your request was successfully accepted</p>
+        </div>
+        <div className={styles.modalCloseBtnFooter}>
+          <button className={styles.btnCloseFooter} onClick={() => setShowModal(false)}>
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default successModal;
+export default successModal;
