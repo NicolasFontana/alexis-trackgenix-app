b442228b78ac368380e303b78b0e45d95c0ed9b1
alejogf <agonzalezfittipaldi@gmail.com>
2022-06-07 00:01:41 -0300

    TG-44: added folders and created files

---
+++ src/Components/Shared/ErrorModal/errormodal.jsx
@@ -0,0 +1,6 @@
+// import React from 'react';
+// import styles from './errormodal.module.css';
+
+// const errorModal
+
+// export default errorModal;
---
+++ src/Components/Shared/SuccessModal/successmodal.jsx
@@ -0,0 +1,6 @@
+// import React from 'react';
+// import styles from './successmodal.module.css';
+
+// const successModal
+
+// export default successModal;
