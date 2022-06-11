a6a6e9f934c14161a6d8c1f55ed47a8ccab3cfbb
Agustin Gomez <agusleongomez@gmail.com>
2022-06-11 13:02:42 -0300

    TG-59: confirm modal added and working

--- src/Components/Tasks/index.js
+++ src/Components/Tasks/index.js
@@ -2,10 +2,19 @@ import React, { useEffect, useState } from 'react';
 import styles from './tasks.module.css';
 import Table from '../Shared/Table/Table';
 import Preloader from '../Shared/Preloader/Preloader';
+import AddButton from '../Shared/Buttons/ButtonAdd';
+import Form from './Form/Form';
+import Modal from '../Shared/ModalForm/index';
+import ConfirmModal from '../Shared/confirmationModal/confirmModal';
 
 function Tasks() {
   const [tasks, saveTasks] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const [idDelete, setIdDelete] = useState(0);
+  let modal;
+  let confirmModal;
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/api/tasks`)
@@ -16,19 +25,48 @@ function Tasks() {
       });
   }, []);
 
-  const delTask = (id) => {
-    const confirmation = confirm('Are you sure you want to delete this task?');
-    if (confirmation) {
-      saveTasks([...tasks.filter((task) => task._id !== id)]);
-      return fetch(`${process.env.REACT_APP_API_URL}/api/tasks/${id}`, {
-        method: 'DELETE'
-      })
-        .then((response) => response.json())
-        .then((response) => {
-          alert('Task deleted successfully', response.msg);
-        });
-    }
+  const delTask = () => {
+    saveTasks([...tasks.filter((task) => task._id !== idDelete)]);
+    return fetch(`${process.env.REACT_APP_API_URL}/api/tasks/${idDelete}`, {
+      method: 'DELETE'
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        alert('Task deleted successfully', response.msg); // aca se llamaria al succes modal
+        closeModal();
+      });
   };
+
+  const addTask = () => {
+    setShowModal(true);
+    modal = (
+      <Modal isOpen={showModal} handleClose={closeModal} title="Add Tasks">
+        <Form />
+      </Modal>
+    );
+  };
+
+  const openConfirmModal = (id) => {
+    setShowConfirmModal(true);
+    setIdDelete(id);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setShowConfirmModal(false);
+  };
+
+  if (showConfirmModal) {
+    confirmModal = (
+      <ConfirmModal
+        isOpen={showConfirmModal}
+        handleClose={closeModal}
+        confirmDelete={delTask}
+        title="Delete Task"
+        message="¿Are you sure you want to delete the task?"
+      ></ConfirmModal>
+    );
+  }
 
   return loading ? (
     <Preloader>
@@ -37,14 +75,16 @@ function Tasks() {
   ) : (
     <section className={styles.container}>
       <h2>TASKS</h2>
+      {modal}
+      {confirmModal}
       <Table
         data={tasks}
         headers={['taskName', 'startDate', 'workedHours', 'description', 'status']}
         titles={['Task Name', 'Start Date', 'Worked Hours', 'Description', 'Status']}
-        delAction={delTask}
+        delAction={openConfirmModal}
         editAction="todavia no hay"
       />
-      <a className={styles.anchor}>Add New Task</a>
+      <AddButton clickAction={addTask}></AddButton>
     </section>
   );
 }
