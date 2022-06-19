a7f5ace1f544c544e19d7b503c44ec00d8875ef6
Iara Criscenti <iaracriscenti@gmail.com>
2022-06-19 14:14:56 -0300

    TG-72: Created index.js file

---
+++ src/Components/Shared/index.js
@@ -0,0 +1,31 @@
+import ButtonAdd from './Buttons/ButtonAdd';
+import ButtonClose from './Buttons/ButtonClose';
+import ButtonDelete from './Buttons/ButtonDelete';
+import ButtonEdit from './Buttons/ButtonEdit';
+import ButtonText from './Buttons/ButtonText';
+import ErrorSuccessModal from './ErrorSuccessModal';
+import ConfirmModal from './confirmationModal/confirmModal';
+import Input from './Input';
+import ModalForm from './ModalForm';
+import NavBar from './NavBar/navbar';
+import Preloader from './Preloader/Preloader';
+import Select from './Select';
+import Table from './Table/Table';
+import Textarea from './Textarea';
+
+export default {
+  ButtonAdd,
+  ButtonClose,
+  ButtonDelete,
+  ButtonEdit,
+  ButtonText,
+  ErrorSuccessModal,
+  ConfirmModal,
+  Input,
+  ModalForm,
+  NavBar,
+  Preloader,
+  Select,
+  Table,
+  Textarea
+};
