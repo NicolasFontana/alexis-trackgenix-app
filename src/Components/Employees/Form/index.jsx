a1dce27e41b794f46ef34262f05b2ef6fa3684f7
Iara Criscenti <iaracriscenti@gmail.com>
2022-06-21 00:32:35 -0300

    TG-80: Updated input props and implemented UseForm

--- src/Components/Employees/Form/index.jsx
+++ src/Components/Employees/Form/index.jsx
@@ -1,161 +1,184 @@
 import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { createEmployee, updateEmployee } from 'redux/employees/thunks';
+// import { useDispatch } from 'react-redux';
+// import { createEmployee, updateEmployee } from 'redux/employees/thunks';
 import styles from './form.module.css';
-import { Input, Select, ButtonText, ErrorSuccessModal } from 'Components/Shared';
+import { Input, Select, ButtonText, ErrorSuccessModal, Textarea } from 'Components/Shared';
+import { useForm } from 'react-hook-form';
 
 const Form = ({ closeModalForm, edit, item }) => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const [showSuccessModal, setShowSuccessModal] = useState(false);
-  const [response, setResponse] = useState('');
-  const [userInput, setUserInput] = useState({
-    firstName: '',
-    lastName: '',
-    phone: '',
-    email: '',
-    password: '',
-    active: '',
-    isProjectManager: '',
-    projects: [],
-    timeSheets: []
-  });
+  // const [response, setResponse] = useState('');
+  // const [userInput, setUserInput] = useState({
+  //   firstName: '',
+  //   lastName: '',
+  //   phone: '',
+  //   email: '',
+  //   password: '',
+  //   active: '',
+  //   isProjectManager: '',
+  //   projects: [],
+  //   timeSheets: []
+  // });
 
   useEffect(() => {
-    if (edit && item._id) {
-      setUserInput({
-        firstName: item.firstName,
-        lastName: item.lastName,
-        phone: item.phone,
-        email: item.email,
-        password: item.password,
-        active: item.active === true ? 'Active' : 'Inactive',
-        isProjectManager: item.isProjectManager === true ? 'Yes' : 'No',
-        projects: item.projects.map((x) => x._id),
-        timeSheets: item.timeSheets.map((x) => x._id)
-      });
-    }
+    // if (edit && item._id) {
+    //   setUserInput({
+    //     firstName: item.firstName,
+    //     lastName: item.lastName,
+    //     phone: item.phone,
+    //     email: item.email,
+    //     password: item.password,
+    //     active: item.active === true ? 'Active' : 'Inactive',
+    //     isProjectManager: item.isProjectManager === true ? 'Yes' : 'No',
+    //     projects: item.projects.map((x) => x._id),
+    //     timeSheets: item.timeSheets.map((x) => x._id)
+    //   });
+    // }
   }, []);
 
-  const onChange = (event) => {
-    setUserInput({ ...userInput, [event.target.name]: event.target.value });
+  const onSubmit = (data) => {
+    if (edit) {
+      (data.isProjectManager = data.isProjectManager == 'Yes' ? true : false), console.log(data);
+    }
+    //   if (
+    //     userInput.firstName === item.firstName &&
+    //     userInput.lastName === item.lastName &&
+    //     userInput.phone === item.phone &&
+    //     userInput.email === item.email &&
+    //     userInput.password === item.password &&
+    //     userInput.active === (item.active === true ? 'Active' : 'Inactive') &&
+    //     userInput.isProjectManager === (item.isProjectManager === true ? 'Yes' : 'No') &&
+    //     userInput.projects.toString() === item.projects.map((x) => x._id).toString() &&
+    //     userInput.timeSheets.toString() === item.timeSheets.map((x) => x._id).toString()
+    //   ) {
+    //     setResponse({ message: "There haven't been string changes", data: {}, error: true });
+    //     setShowSuccessModal(true);
+    //   } else {
+    //     dispatch(updateEmployee(userInput, item._id, setResponse)).then(() => {
+    //       setShowSuccessModal(true);
+    //     });
+    //   }
+    // } else {
+    //   dispatch(createEmployee(userInput, setResponse)).then(() => {
+    //     setShowSuccessModal(true);
+    //   });
+    // }
   };
 
-  const onSubmit = () => {
-    if (edit) {
-      if (
-        userInput.firstName === item.firstName &&
-        userInput.lastName === item.lastName &&
-        userInput.phone === item.phone &&
-        userInput.email === item.email &&
-        userInput.password === item.password &&
-        userInput.active === (item.active === true ? 'Active' : 'Inactive') &&
-        userInput.isProjectManager === (item.isProjectManager === true ? 'Yes' : 'No') &&
-        userInput.projects.toString() === item.projects.map((x) => x._id).toString() &&
-        userInput.timeSheets.toString() === item.timeSheets.map((x) => x._id).toString()
-      ) {
-        setResponse({ message: "There haven't been any changes", data: {}, error: true });
-        setShowSuccessModal(true);
-      } else {
-        dispatch(updateEmployee(userInput, item._id, setResponse)).then(() => {
-          setShowSuccessModal(true);
-        });
-      }
-    } else {
-      dispatch(createEmployee(userInput, setResponse)).then(() => {
-        setShowSuccessModal(true);
-      });
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      firstName: edit ? item.firstName : '',
+      lastName: edit ? item.lastName : '',
+      phone: edit ? item.phone : '',
+      email: edit ? item.email : '',
+      password: edit ? item.password : '',
+      active: edit ? item.active : true,
+      isProjectManager: '',
+      projects: edit ? item.projects.map((x) => x._id) : '',
+      timeSheets: edit ? item.timeSheets.map((x) => x._id) : ''
     }
-  };
+  });
 
   return (
-    <form className={styles.container} onSubmit={onSubmit}>
+    <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
       <Input
         label="First Name"
         name="firstName"
         type="text"
-        value={userInput.firstName}
-        onChange={onChange}
         placeholder="Juan"
+        register={register}
+        // onChange={onChange}
+        error={errors.firstName?.message}
+      />
+      <Textarea
+        label="Projects (separate IDs with a comma)"
+        name="projects"
+        // value={userInput.projects}
+        // onChange={onChange}
+        register={register}
+        error={errors.projects?.message}
       />
       <Input
         label="Last Name"
         name="lastName"
         type="text"
-        value={userInput.lastName}
-        onChange={onChange}
+        // value={userInput.lastName}
+        // onChange={onChange}
         placeholder="Perez"
+        register={register}
+        error={errors.lastName?.message}
       />
       <Input
         label="Phone"
         name="phone"
         type="text"
-        value={userInput.phone}
-        onChange={onChange}
+        // value={userInput.phone}
+        // onChange={onChange}
         placeholder="123456789"
+        register={register}
+        error={errors.phone?.message}
       />
       <Input
         label="Email"
+        register={register}
         name="email"
         type="text"
-        value={userInput.email}
-        onChange={onChange}
+        // value={userInput.email}
+        // onChange={onChange}
         placeholder="juanperez@gmail.com"
+        error={errors.email?.message}
       />
       <Input
         label="Password"
         name="password"
         type="password"
-        value={userInput.password}
-        onChange={onChange}
+        // value={userInput.password}
+        // onChange={onChange}
         placeholder="********"
+        register={register}
+        error={errors.password?.message}
       />
-      <Select
-        label="Active?"
+      <Input
+        label="Active"
         name="active"
-        value={userInput.active}
-        onChange={onChange}
-        title="Define condition"
-        data={['Active', 'Inactive']}
-        required={true}
+        type="checkbox"
+        // checked={userInput.active}
+        register={register}
       />
       <Select
         label="Is a Project Manager?"
         name="isProjectManager"
-        value={userInput.isProjectManager}
-        onChange={onChange}
+        // value={userInput.isProjectManager}
+        // onChange={onChange}
         title="Define PM condition"
         data={['Yes', 'No']}
-        required={true}
-      />
-      <Input
-        label="Projects (separate IDs with a comma)"
-        name="projects"
-        type="text"
-        value={userInput.projects}
-        onChange={onChange}
-        placeholder=""
+        register={register}
+        error={errors.isProjectManager?.message}
       />
       <Input
         label="Timesheets (separate IDs with a comma)"
         name="timeSheets"
         type="text"
-        value={userInput.timeSheets}
-        onChange={onChange}
+        // value={userInput.timeSheets}
+        // onChange={onChange}
         placeholder=""
+        register={register}
+        error={errors.timeSheets?.message}
       />
-      <ButtonText
-        clickAction={() => {
-          onSubmit();
-        }}
-        label={edit ? 'Edit' : 'Create'}
-      />
+      <ButtonText clickAction={handleSubmit(onSubmit)} label={edit ? 'Edit' : 'Create'} />
       <ErrorSuccessModal
         show={showSuccessModal}
         closeModal={() => {
           setShowSuccessModal(false);
         }}
         closeModalForm={closeModalForm}
-        successResponse={response}
+        // successResponse={response}
       />
     </form>
   );
