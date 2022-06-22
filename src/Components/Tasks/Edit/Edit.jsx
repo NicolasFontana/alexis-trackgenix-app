033724f9d08412a286444f18cbd1d8017286438e
LuliBianchini <lulibianchini97@gmail.com>
2022-06-21 22:39:39 -0300

    TG-85: updated edit and add forms

--- src/Components/Tasks/Edit/Edit.jsx
+++ src/Components/Tasks/Edit/Edit.jsx
@@ -1,100 +1,142 @@
+import { joiResolver } from '@hookform/resolvers/joi';
+import Joi from 'joi';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { editTask } from '../../../redux/tasks/thunks';
-import Input from '../../Shared/Input';
-import Select from '../../Shared/Select';
 import Button from '../../Shared/Buttons/ButtonText';
 import MessageModal from '../../Shared/ErrorSuccessModal';
+import Input from '../../Shared/Input';
+import Select from '../../Shared/Select';
 import styles from './edit.module.css';
+
+const taskSchema = Joi.object({
+  taskName: Joi.string()
+    .regex(/^([a-zA-Z]+\s)*[a-zA-Z]+$/)
+    .min(3)
+    .max(50)
+    .required()
+    .messages({
+      'string.min': 'Invalid task name, it must contain more than 3 letters',
+      'string.max': 'Invalid task name, it must not contain more than 50 letters',
+      'any.required': 'Task name is a required field',
+      'string.empty': 'First name is not allowed to be empty',
+      'string.pattern.base':
+        'Must contain only letters and words can only be separated by a single white space'
+    }),
+  startDate: Joi.date().required().messages({ 'any.required': 'Start date is a required field' }),
+  workedHours: Joi.number().integer().min(0).required().messages({
+    'number.integer': 'Invalid number, it must be an integer',
+    'number.min': 'Invalid number, it must be positive',
+    'any.required': 'Worked hours is a required field'
+  }),
+  description: Joi.string().min(6).max(150).required().messages({
+    'string.min': 'Invalid description, it must contain more than 6 letters',
+    'string.max': 'Invalid description, it must not contain more than 150 letters',
+    'any.required': 'Description is a required field'
+  }),
+  status: Joi.string()
+    .min(2)
+    .valid('To do', 'In progress', 'Review', 'Blocked', 'Done', 'Cancelled')
+    .required()
+    .messages({
+      'string.min': 'Invalid status, it must contain more than 2 letters',
+      'any.valid':
+        'Invalid status, it must be one of the following: To do, In progress, Review, Blocked, Done, Cancelled',
+      'any.required': 'Status is a required field'
+    })
+});
 
 const Edit = ({ task, closeModalForm }) => {
   const dispatch = useDispatch();
-
   const [showMessageModal, setShowMessageModal] = useState(false);
   const [message, setMessage] = useState('');
-  const [userInput, setUserInput] = useState({
-    taskName: task.taskName,
-    startDate: task.startDate,
-    workedHours: task.workedHours,
-    description: task.description,
-    status: task.status
-  });
 
-  const onChange = (e) => {
-    setUserInput({ ...userInput, [e.target.name]: e.target.value });
-  };
-
-  const onSubmit = () => {
+  const onSubmit = (data) => {
+    let editedTask = JSON.stringify({
+      taskName: task.taskName,
+      startDate: task.startDate,
+      workedHours: task.workedHours,
+      description: task.description,
+      status: task.status
+    });
     if (
-      userInput.taskName === task.taskName &&
-      userInput.startDate === task.startDate &&
-      userInput.workedHours === task.workedHours &&
-      userInput.description === task.description
+      data.taskName === task.taskName &&
+      data.startDate === task.startDate &&
+      data.workedHours === task.workedHours &&
+      data.description === task.description &&
+      data.status === task.status
     ) {
-      setMessage({ message: "There haven't been any changes", data: {}, error: true });
+      setMessage({ message: "There hasnt't been any changes", data: {}, error: true });
       setShowMessageModal(true);
     } else {
-      dispatch(editTask(userInput, task._id, (response) => setMessage(response))).then(() => {
+      dispatch(editTask(editedTask, task._id, setMessage)).then(() => {
         setShowMessageModal(true);
       });
     }
   };
 
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm({
+    mode: 'onChange',
+    resolver: joiResolver(taskSchema),
+    defaultValues: {
+      taskName: task.taskName,
+      startDate: task.startDate,
+      workedHours: task.workedHours,
+      description: task.description,
+      status: task.status
+    }
+  });
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <Input
         label="Task Name"
         type="text"
         name="taskName"
         placeholder="Insert task name"
-        value={userInput.taskName}
-        onChange={onChange}
-        required={true}
-        register={console.log}
+        register={register}
+        error={errors.taskName?.message}
       />
       <Input
         label="Start Date"
         type="date"
         name="startDate"
-        value={userInput.startDate.substring(0, 10)}
-        onChange={onChange}
-        required={true}
-        register={console.log}
+        register={register}
+        error={errors.startDate?.message}
       />
       <Input
         label="Worked Hours"
         type="text"
         name="workedHours"
         placeholder="Insert hours"
-        value={userInput.workedHours}
-        onChange={onChange}
-        required={true}
-        register={console.log}
+        register={register}
+        error={errors.workedHours?.message}
       />
       <Input
         label="Description"
         type="text"
         name="description"
         placeholder="Insert description"
-        value={userInput.description}
-        onChange={onChange}
-        required={true}
-        register={console.log}
+        register={register}
+        error={errors.description?.message}
       />
       <Select
         label="Status"
         name="status"
-        value={userInput.status}
-        onChange={onChange}
         title="Choose status"
         data={['To do', 'In progress', 'Review', 'Blocked', 'Done', 'Cancelled']}
-        required={true}
-        register={console.log}
+        register={register}
+        error={errors.status?.message}
       />
       <Button clickAction={closeModalForm} label="Cancel">
         Cancel
       </Button>
-      <Button clickAction={onSubmit} label="Submit">
+      <Button clickAction={handleSubmit(onSubmit)} label="Submit">
         Submit
       </Button>
       <MessageModal
--- src/Components/Tasks/Form/Form.jsx
+++ src/Components/Tasks/Form/Form.jsx
@@ -1,85 +1,126 @@
+import { joiResolver } from '@hookform/resolvers/joi';
+import Joi from 'joi';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { addTask } from '../../../redux/tasks/thunks';
-import Input from '../../Shared/Input';
-import Select from '../../Shared/Select';
 import Button from '../../Shared/Buttons/ButtonText';
 import MessageModal from '../../Shared/ErrorSuccessModal';
+import Input from '../../Shared/Input';
+import Select from '../../Shared/Select';
 import styles from './form.module.css';
+
+const taskSchema = Joi.object({
+  taskName: Joi.string()
+    .regex(/^([a-zA-Z]+\s)*[a-zA-Z]+$/)
+    .min(3)
+    .max(50)
+    .required()
+    .messages({
+      'string.min': 'Invalid task name, it must contain more than 3 letters',
+      'string.max': 'Invalid task name, it must not contain more than 50 letters',
+      'any.required': 'Task name is a required field',
+      'string.empty': 'First name is not allowed to be empty',
+      'string.pattern.base':
+        'Must contain only letters and words can only be separated by a single white space'
+    }),
+  startDate: Joi.date().required().messages({ 'any.required': 'Start date is a required field' }),
+  workedHours: Joi.number().integer().min(0).required().messages({
+    'number.integer': 'Invalid number, it must be an integer',
+    'number.min': 'Invalid number, it must be positive',
+    'any.required': 'Worked hours is a required field'
+  }),
+  description: Joi.string().min(6).max(150).required().messages({
+    'string.min': 'Invalid description, it must contain more than 6 letters',
+    'string.max': 'Invalid description, it must not contain more than 150 letters',
+    'any.required': 'Description is a required field'
+  }),
+  status: Joi.string()
+    .min(2)
+    .valid('To do', 'In progress', 'Review', 'Blocked', 'Done', 'Cancelled')
+    .required()
+    .messages({
+      'string.min': 'Invalid status, it must contain more than 2 letters',
+      'any.valid':
+        'Invalid status, it must be one of the following: To do, In progress, Review, Blocked, Done, Cancelled',
+      'any.required': 'Status is a required field'
+    })
+});
 
 const Form = ({ closeModalForm }) => {
   const dispatch = useDispatch();
-
   const [showMessageModal, setShowMessageModal] = useState(false);
   const [message, setMessage] = useState('');
-  const [userInput, setUserInput] = useState({
-    taskName: '',
-    startDate: '',
-    workedHours: '',
-    description: '',
-    status: ''
-  });
 
-  const onChange = (e) => {
-    setUserInput({ ...userInput, [e.target.name]: e.target.value });
-  };
-
-  const onSubmit = () => {
-    dispatch(addTask(userInput, (response) => setMessage(response))).then(() => {
+  const onSubmit = (data) => {
+    let newTask = JSON.stringify({
+      taskName: data.taskName,
+      startDate: data.startDate,
+      workedHours: data.workedHours,
+      description: data.description,
+      status: data.status
+    });
+    dispatch(addTask(newTask, setMessage)).then(() => {
       setShowMessageModal(true);
     });
   };
 
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm({
+    mode: 'onChange',
+    resolver: joiResolver(taskSchema),
+    defaultValues: {
+      taskName: '',
+      startDate: '',
+      workedHours: '',
+      description: '',
+      status: ''
+    }
+  });
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <Input
         label="Task Name"
         type="text"
         name="taskName"
         placeholder="Insert task name"
-        value={userInput.taskName}
-        onChange={onChange}
-        required={true}
-        register={console.log}
+        register={register}
+        error={errors.taskName?.message}
       />
       <Input
         label="Start Date"
         type="date"
         name="startDate"
-        value={userInput.startDate}
-        onChange={onChange}
-        required={true}
-        register={console.log}
+        register={register}
+        error={errors.startDate?.message}
       />
       <Input
         label="Worked Hours"
         type="text"
         name="workedHours"
         placeholder="Insert hours"
-        value={userInput.workedHours}
-        onChange={onChange}
-        required={true}
-        register={console.log}
+        register={register}
+        error={errors.workedHours?.message}
       />
       <Input
         label="Description"
         type="text"
         name="description"
         placeholder="Insert description"
-        value={userInput.description}
-        onChange={onChange}
-        required={true}
-        register={console.log}
+        register={register}
+        error={errors.description?.message}
       />
       <Select
         label="Status"
         name="status"
-        value={userInput.status}
-        onChange={onChange}
         title="Choose status"
         data={['To do', 'In progress', 'Review', 'Blocked', 'Done', 'Cancelled']}
-        required={true}
-        register={console.log}
+        register={register}
+        error={errors.status?.message}
       />
       <Button
         clickAction={() => {
@@ -89,13 +130,8 @@ const Form = ({ closeModalForm }) => {
       >
         Cancel
       </Button>
-      <Button
-        clickAction={() => {
-          onSubmit();
-        }}
-        label="Submit"
-      >
-        Submit
+      <Button clickAction={handleSubmit(onSubmit)} label="Submit">
+        Create
       </Button>
       <MessageModal
         show={showMessageModal}
