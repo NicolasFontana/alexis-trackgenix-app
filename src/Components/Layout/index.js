adb5acb772e8fc36b2bfbf8f7dfb02caab2ee793
Javier Cervera <javieralejandrocervera@gmail.com>
2022-06-22 01:07:17 -0300

    TG-74: fix layout

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import AdminsAdd from '../Admins/Add';
 import AdminsEdit from '../Admins/Edit';
-import Admins from '../Admins/index';
-import Employees from '../Employees/index';
-import EmployeeHome from '../Employees/Home';
 import SuperAdmins from '../SuperAdmins/index';
 import Home from '../Home/index';
 import styles from './layout.module.css';
+import Employees from '../Employees/index';
 import Projects from '../Projects';
 import ProjectsAddMember from '../Projects/Form/AddMember/AddMember';
 import ProjectsForm from '../Projects/Form/index';
 import TasksEdit from '../Tasks/Edit/Edit';
 import TasksForm from '../Tasks/Form/Form';
+import Admins from '../Admins/index';
 import Tasks from '../Tasks/index';
 import TimeSheets from '../TimeSheets';
 import { SideBar, Header, Footer } from 'Components/Shared';
@@ -29,7 +28,6 @@ function Layout() {
           <Route exact path="/admins/edit" component={AdminsEdit} />
           <Route exact path="/super-admins" component={SuperAdmins} />
           <Route exact path="/employees" component={Employees} />
-          <Route exact path="/employee/home" component={EmployeeHome} />
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/projects/form" component={ProjectsForm} />
           <Route exact path="/projects/addmembers" component={ProjectsAddMember} />
