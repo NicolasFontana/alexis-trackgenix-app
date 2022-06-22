dface0228bfbfe98f11123201985ffe0248d4c0a
Iara Criscenti <iaracriscenti@gmail.com>
2022-06-21 21:56:06 -0300

    TG-80: Updated OnSubmit function and fixed validations

--- src/Components/Employees/Form/index.jsx
+++ src/Components/Employees/Form/index.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { createEmployee, updateEmployee } from 'redux/employees/thunks';
+import { useDispatch, useSelector } from 'react-redux';
+import { createEmployee, updateEmployee } from 'redux/employees/thunks';
+import { getProjects } from 'redux/projects/thunks';
+import { getAllTimesheets } from 'redux/time-sheets/thunks';
 import styles from './form.module.css';
-import { Input, Select, ButtonText, ErrorSuccessModal, Textarea } from 'Components/Shared';
+import { Input, Select, ButtonText, ErrorSuccessModal } from 'Components/Shared';
 import { useForm } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/joi';
 import * as Joi from 'joi';
@@ -11,118 +13,101 @@ const schema = Joi.object({
   firstName: Joi.string()
     .min(3)
     .max(50)
-    .pattern(/^[a-zA-Z\s]*$/)
+    .pattern(/(?:\p{L}\p{M}*)+/u)
     .required()
     .messages({
-      'string.min': 'It must contain more than 3 letters',
-      'string.max': 'It must not contain more than 50 letters',
-      'string.pattern.base': 'It must contain only letters',
-      'string.empty': 'First Name is required'
+      'string.min': 'First name must contain more than 3 letters',
+      'string.max': 'First name must not contain more than 50 letters',
+      'string.pattern.base': 'First name must contain only letters',
+      'string.empty': 'First Name is a required field'
     }),
   lastName: Joi.string()
     .min(3)
     .max(50)
-    .pattern(/^[a-zA-Z\s]*$/)
+    .pattern(/(?:\p{L}\p{M}*)+/u)
     .required()
     .messages({
-      'string.min': 'It must contain more than 3 letters',
-      'string.max': 'It must not contain more than 50 letters',
-      'string.pattern.base': 'It must contain only letters',
+      'string.min': 'Last name must contain more than 3 letters',
+      'string.max': 'Last name must not contain more than 50 letters',
+      'string.pattern.base': 'Last name must contain only letters',
       'string.empty': 'Last Name is a required field'
     }),
   phone: Joi.string().pattern(/^\d+$/).length(10).required().messages({
-    'string.pattern.base': 'It must contain only numbers',
-    'string.length': 'It must contain 10 numbers',
+    'string.pattern.base': 'Phone number must contain only numbers',
+    'string.length': 'Phone number must contain 10 numbers',
     'string.empty': 'Phone number is a required field'
   }),
-  email: Joi.string().required().messages({
-    // 'string.email': 'invalid email format',
-    'string.empty': 'Email is a required field'
-  }),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required()
+    .messages({
+      'string.email': 'invalid email format',
+      'string.empty': 'Email is a required field'
+    }),
   password: Joi.string()
     .min(8)
     .pattern(/^(?=.*?[a-zA-Z])(?=.*?[0-9])(?!.*[^a-zA-Z0-9])/)
     .required()
     .messages({
-      'string.min': 'It must contain at least 8 characters',
-      'string.pattern.base': 'It must contain both letters and numbers',
+      'string.min': 'Password must contain at least 8 characters',
+      'string.pattern.base': 'Password must contain both letters and numbers',
       'string.empty': 'Password is a required field'
     }),
-  active: Joi.boolean().required(),
-  isProjectManager: Joi.string().required().messages({ 'string.empty': 'This field is required' }),
-  projects: Joi.array().items(
-    Joi.string().alphanum().length(24).messages({
-      'string.alphanum': 'Invalid project id, it must contain both letters and numbers',
-      'string.length': 'Invalid project id, it must contain 24 characters'
-    })
-  ),
-  timeSheets: Joi.array().items(
-    Joi.string().alphanum().length(24).messages({
-      'string.alphanum': 'Invalid time sheet id, it must contain both letters and numbers',
-      'string.length': 'Invalid time sheet id, it must contain 24 characters'
-    })
-  )
+  active: Joi.string()
+    .valid('Active', 'Inactive')
+    .required()
+    .messages({ 'any.only': 'This is a required field' }),
+  isProjectManager: Joi.string()
+    .valid('Yes', 'No')
+    .required()
+    .messages({ 'any.only': 'This is a required field' }),
+  projects: Joi.string().allow('').alphanum().length(24).messages({
+    'string.alphanum': 'Project id must contain both letters and numbers',
+    'string.length': 'Project id must contain 24 characters'
+  }),
+  timeSheets: Joi.string().allow('').alphanum().length(24).messages({
+    'string.alphanum': 'Timesheet id must contain both letters and numbers',
+    'string.length': 'Timesheet id must contain 24 characters'
+  })
 });
 
 const Form = ({ closeModalForm, edit, item }) => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const [showSuccessModal, setShowSuccessModal] = useState(false);
-  // const [response, setResponse] = useState('');
-  // const [userInput, setUserInput] = useState({
-  //   firstName: '',
-  //   lastName: '',
-  //   phone: '',
-  //   email: '',
-  //   password: '',
-  //   active: '',
-  //   isProjectManager: '',
-  //   projects: [],
-  //   timeSheets: []
-  // });
+  const [response, setResponse] = useState('');
+  const projects = useSelector((state) => state.projects.list);
+  const timeSheets = useSelector((state) => state.timesheets.listTimesheet);
 
   useEffect(() => {
-    // if (edit && item._id) {
-    //   setUserInput({
-    //     firstName: item.firstName,
-    //     lastName: item.lastName,
-    //     phone: item.phone,
-    //     email: item.email,
-    //     password: item.password,
-    //     active: item.active === true ? 'Active' : 'Inactive',
-    //     isProjectManager: item.isProjectManager === true ? 'Yes' : 'No',
-    //     projects: item.projects.map((x) => x._id),
-    //     timeSheets: item.timeSheets.map((x) => x._id)
-    //   });
-    // }
+    dispatch(getProjects()).then(dispatch(getAllTimesheets()));
   }, []);
 
   const onSubmit = (data) => {
     if (edit) {
-      (data.isProjectManager = data.isProjectManager == 'Yes' ? true : false), console.log(data);
+      console.log(data);
+      if (
+        data.firstName === item.firstName &&
+        data.lastName === item.lastName &&
+        data.phone === item.phone &&
+        data.email === item.email &&
+        data.password === item.password &&
+        data.active === (item.active === true ? 'Active' : 'Inactive') &&
+        data.isProjectManager === (item.isProjectManager === true ? 'Yes' : 'No') &&
+        data.projects === item.projects.map((x) => x._id).toString() &&
+        data.timeSheets === item.timeSheets.map((x) => x._id).toString()
+      ) {
+        setResponse({ message: "There haven't been string changes", data: {}, error: true });
+        setShowSuccessModal(true);
+      } else {
+        dispatch(updateEmployee(data, item._id, setResponse)).then(() => {
+          setShowSuccessModal(true);
+        });
+      }
+    } else {
+      dispatch(createEmployee(data, setResponse)).then(() => {
+        setShowSuccessModal(true);
+      });
     }
-    //   if (
-    //     userInput.firstName === item.firstName &&
-    //     userInput.lastName === item.lastName &&
-    //     userInput.phone === item.phone &&
-    //     userInput.email === item.email &&
-    //     userInput.password === item.password &&
-    //     userInput.active === (item.active === true ? 'Active' : 'Inactive') &&
-    //     userInput.isProjectManager === (item.isProjectManager === true ? 'Yes' : 'No') &&
-    //     userInput.projects.toString() === item.projects.map((x) => x._id).toString() &&
-    //     userInput.timeSheets.toString() === item.timeSheets.map((x) => x._id).toString()
-    //   ) {
-    //     setResponse({ message: "There haven't been string changes", data: {}, error: true });
-    //     setShowSuccessModal(true);
-    //   } else {
-    //     dispatch(updateEmployee(userInput, item._id, setResponse)).then(() => {
-    //       setShowSuccessModal(true);
-    //     });
-    //   }
-    // } else {
-    //   dispatch(createEmployee(userInput, setResponse)).then(() => {
-    //     setShowSuccessModal(true);
-    //   });
-    // }
   };
 
   const {
@@ -138,10 +123,10 @@ const Form = ({ closeModalForm, edit, item }) => {
       phone: edit ? item.phone : '',
       email: edit ? item.email : '',
       password: edit ? item.password : '',
-      active: edit ? item.active : true,
-      isProjectManager: '',
-      projects: edit ? item.projects.map((x) => x._id) : '',
-      timeSheets: edit ? item.timeSheets.map((x) => x._id) : ''
+      active: edit ? (item.active ? 'Active' : 'Inactive') : '',
+      isProjectManager: edit ? (item.isProjectManager ? 'Yes' : 'No') : '',
+      projects: edit ? item.projects[0]._id : '',
+      timeSheets: edit ? item.timeSheets[0]._id : ''
     }
   });
 
@@ -153,23 +138,12 @@ const Form = ({ closeModalForm, edit, item }) => {
         type="text"
         placeholder="Juan"
         register={register}
-        // onChange={onChange}
         error={errors.firstName?.message}
-      />
-      <Textarea
-        label="Projects (separate IDs with a comma)"
-        name="projects"
-        // value={userInput.projects}
-        // onChange={onChange}
-        register={register}
-        error={errors.projects?.message}
       />
       <Input
         label="Last Name"
         name="lastName"
         type="text"
-        // value={userInput.lastName}
-        // onChange={onChange}
         placeholder="Perez"
         register={register}
         error={errors.lastName?.message}
@@ -178,8 +152,6 @@ const Form = ({ closeModalForm, edit, item }) => {
         label="Phone"
         name="phone"
         type="text"
-        // value={userInput.phone}
-        // onChange={onChange}
         placeholder="123456789"
         register={register}
         error={errors.phone?.message}
@@ -189,8 +161,6 @@ const Form = ({ closeModalForm, edit, item }) => {
         register={register}
         name="email"
         type="text"
-        // value={userInput.email}
-        // onChange={onChange}
         placeholder="juanperez@gmail.com"
         error={errors.email?.message}
       />
@@ -198,38 +168,47 @@ const Form = ({ closeModalForm, edit, item }) => {
         label="Password"
         name="password"
         type="password"
-        // value={userInput.password}
-        // onChange={onChange}
         placeholder="********"
         register={register}
         error={errors.password?.message}
       />
-      <Input
-        label="Active"
+      <Select
+        label="Active?"
         name="active"
-        type="checkbox"
-        // checked={userInput.active}
+        title="Define condition"
+        data={['Active', 'Inactive']}
         register={register}
+        error={errors.active?.message}
       />
       <Select
         label="Is a Project Manager?"
         name="isProjectManager"
-        // value={userInput.isProjectManager}
-        // onChange={onChange}
         title="Define PM condition"
         data={['Yes', 'No']}
         register={register}
         error={errors.isProjectManager?.message}
       />
-      <Input
-        label="Timesheets (separate IDs with a comma)"
+      <Select
+        label="Timesheets"
         name="timeSheets"
-        type="text"
-        // value={userInput.timeSheets}
-        // onChange={onChange}
-        placeholder=""
+        title="Choose timesheet"
+        data={timeSheets.map((timesheet) => ({
+          _id: timesheet._id,
+          optionText: timesheet._id
+        }))}
         register={register}
         error={errors.timeSheets?.message}
+      />
+      <Select
+        label="Projects"
+        name="projects"
+        title="Choose Project"
+        data={projects.map((project) => ({
+          _id: project._id,
+          optionText: project.name
+        }))}
+        register={register}
+        error={errors.projects?.message}
       />
       <ButtonText clickAction={handleSubmit(onSubmit)} label={edit ? 'Edit' : 'Create'} />
       <ErrorSuccessModal
@@ -238,7 +217,7 @@ const Form = ({ closeModalForm, edit, item }) => {
           setShowSuccessModal(false);
         }}
         closeModalForm={closeModalForm}
-        // successResponse={response}
+        successResponse={response}
       />
     </form>
   );
