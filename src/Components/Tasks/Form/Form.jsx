e5b9b2ffa59155444b262c9e302bbd565674720b
LuliBianchini <lulibianchini97@gmail.com>
2022-06-24 11:44:52 -0300

    TG-85: Validations pattern and messages modifications

--- src/Components/Tasks/Form/Form.jsx
+++ src/Components/Tasks/Form/Form.jsx
@@ -16,8 +16,7 @@ const taskSchema = Joi.object({
     .messages({
       'string.min': 'Invalid task name, it must contain more than 3 letters',
       'string.max': 'Invalid task name, it must not contain more than 50 letters',
-      'any.required': 'Task name is a required field',
-      'string.empty': 'First name is not allowed to be empty',
+      'string.empty': 'Task name is a required field',
       'string.pattern.base':
         'Must contain only letters and words can only be separated by a single white space'
     }),
@@ -25,12 +24,12 @@ const taskSchema = Joi.object({
   workedHours: Joi.number().integer().min(0).required().messages({
     'number.integer': 'Invalid number, it must be an integer',
     'number.min': 'Invalid number, it must be positive',
-    'any.required': 'Worked hours is a required field'
+    'number.base': 'Worked hours is a required field'
   }),
   description: Joi.string().min(6).max(150).required().messages({
     'string.min': 'Invalid description, it must contain more than 6 letters',
     'string.max': 'Invalid description, it must not contain more than 150 letters',
-    'any.required': 'Description is a required field'
+    'string.empty': 'Description is a required field'
   }),
   status: Joi.string()
     .min(2)
@@ -40,7 +39,7 @@ const taskSchema = Joi.object({
       'string.min': 'Invalid status, it must contain more than 2 letters',
       'any.valid':
         'Invalid status, it must be one of the following: To do, In progress, Review, Blocked, Done, Cancelled',
-      'any.required': 'Status is a required field'
+      'string.empty': 'Status is a required field'
     })
 });
 
