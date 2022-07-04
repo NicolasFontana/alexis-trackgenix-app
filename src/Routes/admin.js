8bd3e4e812c8dae6f75d81eab9449fe663184449
Iara Criscenti <iaracriscenti@gmail.com>
2022-07-04 18:00:41 -0300

    TG-102: Removed extra components and routes

--- src/Routes/admin.js
+++ src/Routes/admin.js
@@ -2,35 +2,19 @@ import React from 'react';
 import { Switch, Route, useRouteMatch, Redirect } from 'react-router-dom';
 import Home from 'Components/Admin/Home';
 import Layout from 'Components/Layout';
-import Admins from 'Components/Admin/Admins';
-import SuperAdmins from 'Components/Admin/SuperAdmins';
 import Employees from 'Components/Admin/Employees';
 import Projects from 'Components/Admin/Projects';
-import Timesheets from 'Components/Admin/TimeSheets';
-import Tasks from 'Components/Admin/Tasks';
-import {
-  faClock,
-  faFolderClosed,
-  faHouse,
-  faListCheck,
-  faUsers
-} from '@fortawesome/free-solid-svg-icons';
+import { faFolderClosed, faHouse, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const home = <FontAwesomeIcon icon={faHouse}></FontAwesomeIcon>;
 const users = <FontAwesomeIcon icon={faUsers}></FontAwesomeIcon>;
 const projects = <FontAwesomeIcon icon={faFolderClosed}></FontAwesomeIcon>;
-const tasks = <FontAwesomeIcon icon={faListCheck}></FontAwesomeIcon>;
-const timesheets = <FontAwesomeIcon icon={faClock}></FontAwesomeIcon>;
 
 const adminsRoutes = [
   { icon: home, name: 'Home', path: '/admin' },
-  { icon: users, name: 'Admins', path: '/admin/admins' },
-  { icon: users, name: 'Super-Admins', path: '/admin/super-admins' },
   { icon: users, name: 'Employees', path: '/admin/employees' },
-  { icon: projects, name: 'Projects', path: '/admin/projects' },
-  { icon: timesheets, name: 'Time-Sheet', path: '/admin/time-sheets' },
-  { icon: tasks, name: 'Tasks', path: '/admin/tasks' }
+  { icon: projects, name: 'Projects', path: '/admin/projects' }
 ];
 
 const Admin = () => {
@@ -38,12 +22,8 @@ const Admin = () => {
   return (
     <Layout routes={adminsRoutes} logout>
       <Switch>
-        <Route exact path={`${url}/admins`} component={Admins} />
-        <Route exact path={`${url}/super-admins`} component={SuperAdmins} />
         <Route exact path={`${url}/employees`} component={Employees} />
         <Route exact path={`${url}/projects`} component={Projects} />
-        <Route exact path={`${url}/time-sheets`} component={Timesheets} />
-        <Route exact path={`${url}/tasks`} component={Tasks} />
         <Route exact path={`${url}/`} component={Home} />
         <Redirect to={`${url}`} />
       </Switch>
