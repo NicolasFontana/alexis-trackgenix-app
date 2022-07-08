ba84287b566e68df84d1fb4e484374062fcf3ca7
Iara Criscenti <iaracriscenti@gmail.com>
2022-07-08 13:56:16 -0300

    TG-102: Moved deleted folders from Admin to Employee

---
+++ src/Components/Employee/TimeSheets - 0/FormEdit/index.jsx
@@ -0,0 +1,94 @@
+import { React, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { Select, ButtonText, ErrorSuccessModal, Input } from 'Components/Shared';
+import { putTimesheet } from 'redux/time-sheets/thunks';
+import styles from './form.module.css';
+
+const FormEdit = ({ closeModalEdit, timesheetItem }) => {
+  const dispatch = useDispatch();
+  const [message, setMessage] = useState('');
+  const [showMessageModal, setShowMessageModal] = useState(false);
+  const listProject = useSelector((state) => state.projects.list);
+  const listTask = useSelector((state) => state.tasks.list);
+
+  const onSubmit = (data) => {
+    let userInput = {
+      projectId: data.projectId,
+      task: data.task,
+      approved: data.approved
+    };
+    if (
+      userInput.projectId == timesheetItem.projectId._id &&
+      userInput.task == timesheetItem.Task[0].taskId._id &&
+      userInput.approved == timesheetItem.approved
+    ) {
+      setMessage({ message: "There haven't been any changes", data: {}, error: true });
+      setShowMessageModal(true);
+    } else {
+      dispatch(putTimesheet(userInput, timesheetItem._id, setMessage));
+      setShowMessageModal(true);
+    }
+  };
+
+  const closeMessageModal = () => {
+    setShowMessageModal(false);
+  };
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm({
+    mode: 'onBlur',
+    defaultValues: {
+      projectId: timesheetItem.projectId._id,
+      task: timesheetItem.Task[0].taskId._id,
+      approved: timesheetItem.approved
+    }
+  });
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <Select
+        label="Projects"
+        name="projectId"
+        title="Choose project"
+        data={listProject.map((project) => ({
+          _id: project._id,
+          optionText: project.name
+        }))}
+        register={register}
+        error={errors.projectId?.message}
+      />
+      <Select
+        label="Tasks"
+        name="task"
+        title="Choose task"
+        data={listTask.map((task) => ({
+          _id: task._id,
+          optionText: task.taskName
+        }))}
+        register={register}
+        error={errors.task?.message}
+      />
+      <Input
+        label="Approved"
+        name="approved"
+        title="Approve"
+        type="checkbox"
+        register={register}
+        error={errors.approved?.message}
+      />
+      <ButtonText clickAction={handleSubmit(onSubmit)} label="Edit" />
+      <ErrorSuccessModal
+        show={showMessageModal}
+        closeModal={closeMessageModal}
+        closeModalForm={closeModalEdit}
+        successResponse={message}
+      />
+    </form>
+  );
+};
+
+export default FormEdit;
