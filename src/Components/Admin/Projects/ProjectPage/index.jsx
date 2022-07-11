124ffe89cb9894c9d1b9104f9ed14a3371ef2add
Javier Cervera <javieralejandrocervera@gmail.com>
2022-07-11 16:50:35 -0300

    TG-103: add project info section

--- src/Components/Admin/Projects/ProjectPage/index.jsx
+++ src/Components/Admin/Projects/ProjectPage/index.jsx
@@ -3,58 +3,58 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 // import { getProjectById } from 'redux/projects/thunks';
 import { useParams } from 'react-router-dom';
-import { getEmployees } from 'redux/employees/thunks';
+// import { getEmployees } from 'redux/employees/thunks';
 import { getProjects } from 'redux/projects/thunks';
 import styles from './projectPage.module.css';
+import { Preloader } from 'Components/Shared';
 
 const ProjectPage = () => {
   const dispatch = useDispatch();
-  const projects = useSelector((state) => state.projects.list);
-  const employees = useSelector((state) => state.employees.list);
   const { id } = useParams();
-  let employee;
+  const project = useSelector((state) => state.projects.list).find((project) => project._id === id);
+  const isLoading = useSelector((state) => state.projects.isLoading);
+  // const employees = useSelector((state) => state.employees.list);
+  let pm = project?.members.find((member) => member.role === 'PM');
+  // console.log(project);
 
   useEffect(() => {
-    dispatch(getEmployees());
     dispatch(getProjects());
+    // dispatch(getEmployees());
   }, []);
 
-  if (employees && projects) {
-    const employeeProjects = projects.filter((project) => {
-      project.members.find((member) => member._id === id);
-    });
-    employee = employees.find((element) => element._id === id);
-    console.log(employee);
-    console.log(employeeProjects);
-  }
-
-  return (
+  return isLoading ? (
+    <section className={styles.containerPreloader}>
+      <Preloader>
+        <p>Loading Projects</p>
+      </Preloader>
+    </section>
+  ) : (
     <section className={styles.container}>
       <h2 className={styles.title}>Project Page</h2>
       <div className={styles.box}>
         <div className={styles.field}>
           <h3>Project Name</h3>
-          <p>a</p>
+          <p>{project?.name}</p>
         </div>
         <div className={styles.field}>
           <h3>Client</h3>
-          <p>a</p>
+          <p>{project?.clientName}</p>
         </div>
         <div className={styles.field}>
           <h3>PM</h3>
-          <p>a</p>
+          <p>{pm ? `${pm.employeeId?.firstName} ${pm.employeeId?.lastName}` : 'To be defined'}</p>
         </div>
         <div className={styles.field}>
           <h3>Description</h3>
-          <p>a</p>
+          <p>{project?.description}</p>
         </div>
         <div className={styles.field}>
           <h3>Start Date</h3>
-          <p>a</p>
+          <p>{project?.startDate?.slice(0, 10)}</p>
         </div>
         <div className={styles.field}>
           <h3>Active</h3>
-          <p>a</p>
+          <p>{project?.active ? 'Active' : 'Inactive'}</p>
         </div>
       </div>
     </section>
