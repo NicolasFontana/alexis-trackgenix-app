a72f20782b68806caad0ec2688ac609f70a79036
Agustin Gomez <agusleongomez@gmail.com>
2022-07-13 17:04:03 -0300

    TG-104: table of projects added to employee page

--- src/Components/Admin/Employees/EmployeePage/index.jsx
+++ src/Components/Admin/Employees/EmployeePage/index.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getEmployees } from 'redux/employees/thunks';
 import { getProjects } from 'redux/projects/thunks';
-import { Select } from 'Components/Shared';
+import { Table } from 'Components/Shared';
 import styles from 'Components/Admin/Employees/EmployeePage/employeePage.module.css';
 
 const EmployeePage = () => {
@@ -12,6 +12,7 @@ const EmployeePage = () => {
   const employees = useSelector((state) => state.employees.list);
   const projects = useSelector((state) => state.projects.list);
   let employee;
+  let employeeProjects = []; //the data of the projects will be loaded here
 
   useEffect(() => {
     dispatch(getEmployees());
@@ -20,17 +21,23 @@ const EmployeePage = () => {
 
   if (employees && projects) {
     const { id } = useParams();
-    const employeeProjects = projects.filter((project) => {
-      project.members.find((member) => member._id === id);
-    });
     employee = employees.find((element) => element._id === id);
-    console.log(employee);
-    console.log(employeeProjects);
+    //employeeProjects is filled
+    projects.forEach((project) => {
+      project.members.forEach((member) => {
+        if (member.employeeId._id === employee._id) {
+          employeeProjects.push({
+            name: project.name,
+            role: member.role,
+            rate: member.rate
+          });
+        }
+      });
+    });
   }
 
   return (
     <section className={styles.container}>
-      <h2 className={styles.title}>Employee Page</h2>
       <div className={styles.box}>
         <div className={styles.field}>
           <h3>First Name</h3>
@@ -64,24 +71,15 @@ const EmployeePage = () => {
           <h3>Status</h3>
           <p>{employee.status ? employee.status : 'was not loaded'}</p>
         </div>
-        <div className={styles.field}>
-          <h3>Rate</h3>
-          <p>a</p>
-        </div>
-        <div className={styles.field}>
-          <h3>Role</h3>
-          <p>a</p>
-        </div>
-        <div className={styles.select}>
-          <Select
-            title="Projects"
-            defaultValue=""
-            data={projects}
-            disabled
-            register={console.log}
-          />
-        </div>
       </div>
+      <Table
+        data={employeeProjects}
+        headers={['name', 'role', 'rate']}
+        titles={['Name', 'Role', 'Rate']}
+        modifiers={{}}
+        delAction={() => console.log('hola')}
+        editAction={() => console.log('hola')}
+      />
     </section>
   );
 };
