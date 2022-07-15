11ee11fe6c4d3d90e9216d4e13c7f67d91babcf4
Iara Criscenti <iaracriscenti@gmail.com>
2022-07-15 14:18:35 -0300

    TG-113: Added logic to show PM table

--- src/Components/Employee/Projects/index.jsx
+++ src/Components/Employee/Projects/index.jsx
@@ -1,44 +1,60 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Table } from 'Components/Shared';
 import styles from './projects.module.css';
+import { getProjects } from 'redux/projects/thunks';
+import { useEffect } from 'react';
 
 function Projects() {
-  const employeeId = useSelector((state) => state.auth.user?.data._id);
-  const employee = useSelector((state) => state.employees.list).find(
-    (employee) => employee._id === employeeId
+  const dispatch = useDispatch();
+  const employee = useSelector((state) => state.auth.user?.data);
+  const projects = useSelector((state) => state.projects.list).filter((project) =>
+    employee?.projects.some((employeeProject) => employeeProject === project._id)
   );
-  const projects = useSelector((state) => state.projects.list).filter((listProject) =>
-    employee?.projects.some((employeeProject) => employeeProject._id === listProject._id)
+  const projectsPM = projects.filter((project) =>
+    project.members.find((member) => member.employeeId._id === employee._id && member.role === 'PM')
   );
 
-  projects.forEach(
-    (project) =>
-      (project.employeeRole = project.members.find((e) => e.employeeId._id === employeeId)?.role)
-  );
+  useEffect(() => {
+    dispatch(getProjects());
+  }, []);
 
   return (
-    <section className={styles.container}>
-      <h2 className={styles.title}>Employee Projects</h2>
-      <Table
-        data={projects}
-        headers={[
-          'name',
-          'clientName',
-          'startDate',
-          'endDate',
-          'active',
-          'employeeRole',
-          'members'
-        ]}
-        titles={['Project', 'Client Name', 'Start Date', 'End Date', 'Active', 'Role', 'Rate']}
-        modifiers={{
-          startDate: (x) => x?.slice(0, 10),
-          endDate: (x) => x?.slice(0, 10),
-          active: (x) => (x ? 'Active' : 'Inactive'),
-          members: (x) => x.find((e) => e.employeeId._id === employeeId)?.rate
-        }}
-      />
-    </section>
+    <>
+      {projectsPM.length ? (
+        <section className={styles.container}>
+          <h2 className={styles.title}>PM Projects</h2>
+          <Table
+            data={projectsPM}
+            headers={['name', 'clientName', 'startDate', 'endDate', 'active']}
+            titles={['Project name', 'Client', 'Start Date', 'End Date', 'Active']}
+            modifiers={{
+              startDate: (x) => x?.slice(0, 10),
+              endDate: (x) => x?.slice(0, 10),
+              active: (x) => (x ? 'Active' : 'Inactive')
+            }}
+            editAction={console.log}
+          />
+        </section>
+      ) : null}
+      <section className={styles.container}>
+        <h2 className={styles.title}>All Projects</h2>
+        {projects.length ? (
+          <Table
+            data={projects}
+            headers={['name', 'clientName', 'startDate', 'endDate', 'active', 'members']}
+            titles={['Project name', 'Client', 'Start Date', 'End Date', 'Active', 'Role']}
+            modifiers={{
+              startDate: (x) => x?.slice(0, 10),
+              endDate: (x) => x?.slice(0, 10),
+              active: (x) => (x ? 'Active' : 'Inactive'),
+              members: (x) => x.find((e) => e.employeeId._id === employee._id)?.role
+            }}
+          />
+        ) : (
+          <p> You have not been assigned to any project yet </p>
+        )}
+      </section>
+    </>
   );
 }
 
