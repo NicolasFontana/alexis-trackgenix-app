e954e3b8b886873a5741c3cd814ac0d11aa69f71
Javier Cervera <javieralejandrocervera@gmail.com>
2022-07-19 19:11:24 -0300

    TG-116: add tasks page

---
+++ src/Components/Employee/Projects/ProjectPage/MemberTimesheetPage/MemberTasksPage/index.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useHistory, generatePath } from 'react-router-dom';
+import { getEmployees } from 'redux/employees/thunks';
+import { getAllTimesheets } from 'redux/time-sheets/thunks';
+import styles from './memberTasksPage.module.css';
+import {
+  Preloader,
+  Table,
+  // ModalForm,
+  // ConfirmModal,
+  // ErrorSuccessModal,
+  ButtonText
+} from 'Components/Shared';
+
+const MemberTasksPage = () => {
+  const dispatch = useDispatch();
+  let history = useHistory();
+  const { id, memberId, timesheetId } = useParams();
+  const isLoading = useSelector((state) => state.employees.isLoading);
+  const member = useSelector((state) => state.employees.list).find(
+    (employee) => employee._id === memberId
+  );
+  const timesheet = useSelector((state) => state.timesheets.listTimesheet).find(
+    (timesheet) => timesheet._id === timesheetId
+  );
+
+  useEffect(() => {
+    dispatch(getEmployees());
+    dispatch(getAllTimesheets());
+  }, []);
+
+  // console.log(member);
+  console.log(timesheet);
+
+  return isLoading ? (
+    <section className={styles.containerPreloader}>
+      <Preloader>
+        <p>Loading Project</p>
+      </Preloader>
+    </section>
+  ) : (
+    <section className={styles.container}>
+      <ButtonText
+        label="Go back"
+        clickAction={() => {
+          history.push(
+            generatePath('/employee/projects/:id/members/:memberId', { id: id, memberId: memberId })
+          );
+        }}
+      ></ButtonText>
+      <h2>Member: {`${member?.firstName} ${member?.lastName}`}</h2>
+      <h3>Project: {member?.projects.find((project) => project._id === id).name} </h3>
+      <Table
+        data={timesheet?.Task.map((task) => task.taskId)}
+        headers={['taskName', 'description', 'startDate', 'status', 'workedHours']}
+        titles={['Task Name', 'Description', 'Start Date', 'Status', 'Worked Hours']}
+      />
+    </section>
+  );
+};
+
+export default MemberTasksPage;
