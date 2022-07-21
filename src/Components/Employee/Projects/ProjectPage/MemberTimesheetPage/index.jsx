2e50c1acdfb52d2715ac16d2419fc17c9fe095c8
Javier Cervera <javieralejandrocervera@gmail.com>
2022-07-21 13:38:19 -0300

    TG-116: add empty tables logic

--- src/Components/Employee/Projects/ProjectPage/MemberTimesheetPage/index.jsx
+++ src/Components/Employee/Projects/ProjectPage/MemberTimesheetPage/index.jsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useHistory, generatePath } from 'react-router-dom';
 import { getEmployees } from 'redux/employees/thunks';
-// import { getAllTimesheets } from 'redux/time-sheets/thunks';
 import styles from './memberTimesheetPage.module.css';
 import {
   Preloader,
@@ -22,11 +21,7 @@ const MemberTimesheetPage = () => {
   const member = useSelector((state) => state.employees.list).find(
     (employee) => employee._id === memberId
   );
-  // const timesheets = useSelector((state) => state.timesheets.listTimesheet).filter(
-  //   (timesheet) =>
-  //     member?.timeSheets.some((memberTimesheet) => memberTimesheet._id === timesheet._id) &&
-  //     timesheet.projectId._id === id
-  // );
+
   const redirectAction = (timesheetId) => {
     history.push(
       generatePath('/employee/projects/:id/members/:memberId/timesheets/:timesheetId', {
@@ -39,11 +34,7 @@ const MemberTimesheetPage = () => {
 
   useEffect(() => {
     dispatch(getEmployees());
-    // dispatch(getAllTimesheets());
   }, []);
-
-  // console.log(member);
-  // console.log(timesheets);
 
   return isLoading ? (
     <section className={styles.containerPreloader}>
@@ -61,16 +52,20 @@ const MemberTimesheetPage = () => {
       ></ButtonText>
       <h2>Member Timesheets</h2>
       <h3>{`${member?.firstName} ${member?.lastName}`}</h3>
-      <Table
-        data={member?.timeSheets}
-        headers={['projectId', '', 'approved']}
-        titles={['Project', 'Worked hours', 'Approved']}
-        modifiers={{
-          projectId: (x) => member.projects.find((project) => project._id === x).name,
-          approved: (x) => (x ? 'Approved' : 'Not approved')
-        }}
-        redirect={redirectAction}
-      />
+      {member?.timeSheets.length !== 0 ? (
+        <Table
+          data={member?.timeSheets}
+          headers={['projectId', '', 'approved']}
+          titles={['Project', 'Worked hours', 'Approved']}
+          modifiers={{
+            projectId: (x) => member.projects.find((project) => project._id === x).name,
+            approved: (x) => (x ? 'Approved' : 'Not approved')
+          }}
+          redirect={redirectAction}
+        />
+      ) : (
+        <p>No timesheets have been uploaded for this project</p>
+      )}
     </section>
   );
 };
