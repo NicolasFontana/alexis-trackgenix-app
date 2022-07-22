f4e541ccf7f65856b9e08360134fcf245cbf815c
Agustin Gomez <agusleongomez@gmail.com>
2022-07-22 20:24:37 -0300

    TG-115: header title redirect fixed

--- src/Components/Shared/Header/index.js
+++ src/Components/Shared/Header/index.js
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = ({ sidebarOpener }) => {
   const location = useLocation();
@@ -10,6 +11,13 @@ const Header = ({ sidebarOpener }) => {
   const userIcon = <FontAwesomeIcon icon={faCircleUser}></FontAwesomeIcon>;
 
   let user;
+  //The title redirect to this url
+  let url =
+    location.pathname.substring(0, 6) === '/admin'
+      ? '/admin'
+      : location.pathname.substring(0, 9) === '/employee'
+      ? '/employee'
+      : '/home';
 
   //This comparison is used to show or hide the profile button
   if (
@@ -38,7 +46,9 @@ const Header = ({ sidebarOpener }) => {
           <div className={styles.bar2}></div>
           <div className={styles.bar3}></div>
         </div>
-        <h2 className={styles.title}>Trackgenix</h2>
+        <h2 className={styles.title}>
+          <Link to={url}>Trackgenix</Link>
+        </h2>
       </div>
       <div>{user}</div>
     </header>
